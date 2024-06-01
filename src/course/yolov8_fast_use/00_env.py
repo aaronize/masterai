609d3bf15a449cd8eceda6f4c17c20530393b4ea
aaronize <chendai2004@hotmail.com>
2024-06-01 23:44:11 +0800

    feat

---
+++ src/course/yolov8_fast_use/00_env.py
@@ -0,0 +1,9 @@
+import cv2
+import torch
+import ultralytics
+
+
+if __name__ == '__main__':
+    print(cv2.__version__)
+    print(torch.__version__)
+    print(ultralytics.__version__)
---
+++ src/course/yolov8_fast_use/01_cls_demo.py
@@ -0,0 +1,29 @@
+import cv2
+
+from ultralytics import YOLO
+
+
+def main():
+    """
+    使用预训练模型
+    :return:
+    """
+    # 加载模型
+    model = YOLO(model="../../../models/yolov8l-cls.pt")
+
+    # 打开图像
+    img = cv2.imread(filename="./iris.png")
+
+    # 推理过程
+    results = model.predict(source=img)
+
+    # 绘制结果
+    img = results[0].plot()
+    cv2.imshow(winname="demo", mat=img)
+
+    if cv2.waitKey(delay=30000) == 27:
+        pass
+
+
+if __name__ == '__main__':
+    main()
