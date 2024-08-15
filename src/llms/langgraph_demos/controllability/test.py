e75340fd1685716c0390184d22998b9c693bae12
aaronize <chendai2004@hotmail.com>
2024-08-15 23:52:00 +0800

    feat

--- src/llms/langgraph_demos/controllability/test.py
+++ src/llms/langgraph_demos/controllability/test.py
@@ -1,7 +1,13 @@
 from typing import TypedDict, Annotated
-
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
 from langgraph.graph import StateGraph
+from langgraph.prebuilt import create_react_agent
 
+toolkit = SQLDatabaseToolkit(db=None, llm=None)
+tools = toolkit.get_tools()
+
+agent_executor = create_react_agent()
+agent_executor.invoke()
 
 def reduce_list(left: list | None, right: list | None) -> list:
     if not left:
